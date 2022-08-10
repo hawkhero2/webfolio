56d15ecd79983d9342a4f1fdd10a6c1ae234c681
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2022-08-10 15:07:41 +0300

    removed dotenv require

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-require('dotenv').config()
+
 
 const Home: NextPage = () => {
   return (
@@ -30,9 +30,9 @@ const Home: NextPage = () => {
           </p>
           {/* TODO: add image for github project*/}
           <Image
-           src="public/">
-
-           </Image>
+           src="public/"
+           
+          ></Image>
         </div>
       </main>
 
